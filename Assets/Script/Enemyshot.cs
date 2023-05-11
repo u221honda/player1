16419b505bd91ff944f06d7eb03935e26bc7104e
u221honda <22111326ih@tama.ac.jp>
2023-05-11 12:48:06 +0900

    scriptを入れた

---
+++ Assets/Script/Enemyshot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject ball;
+    private float ballSpeed = 10.0f;
+    private float time = 1.0f;
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            BallShot();
+            time = 1.0f;
+        }
+    }
+ 
+    void BallShot()
+    {
+        GameObject shotObj = Instantiate(ball, transform.position, Quaternion.identity);
+        shotObj.GetComponent<Rigidbody>().velocity = transform.forward * ballSpeed;
+    }
+}
