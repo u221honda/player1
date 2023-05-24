99933a81af2b818cf150a6f770c740f1ce1bff68
u221honda <22111326ih@tama.ac.jp>
2023-05-24 19:37:50 +0900

    Playerに向かって玉を発射しないように設定

--- Assets/Script/Enemyshot.cs
+++ Assets/Script/Enemyshot.cs
@@ -9,23 +9,28 @@ public class EnemyShot : MonoBehaviour
     private float ballSpeed = 10.0f;
     private float time = 1.0f;
 
+    private int vecX;
+    private int vecY;
     void Update()
     {
-        transform.LookAt(player.transform);
+        //transform.LookAt(player.transform);//ここを編集したら玉がplayerに向かわなくなるはず
         time -= Time.deltaTime;
         if(time <= 0)
         {
+           
+           vecX = Random.Range(-3,3);
+           vecY = Random.Range(-3,3);
             BallShot();
-            time = 1.0f;
+            time = 1.0f;//ballの速度
         }
     }
  
     void BallShot()
     {
         //新しいボールを生成して、発射する
-        GameObject shotObj = Instantiate(ball, transform.position, Quaternion.identity);
+        GameObject shotObj = Instantiate(ball,new Vector3(vecX,vecY,5) , Quaternion.identity);
         shotObj.GetComponent<Rigidbody>().velocity = transform.forward * ballSpeed;
         //Objectを消す関数
-       Destroy(shotObj, 5f);                                                                       
+       Destroy(shotObj, 10f);                                                                       
     }
 }
