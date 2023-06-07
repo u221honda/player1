2b8c87024410f4f84b9a41ad17c9ffa958277a88
u221honda <22111326ih@tama.ac.jp>
2023-06-07 19:38:31 +0900

    とりあえずOAKに関係ありそうなscriptを入れてみた

--- Assets/Script/Enemyshot.cs
+++ Assets/Script/Enemyshot.cs
@@ -9,24 +9,26 @@ public class EnemyShot : MonoBehaviour
     private float ballSpeed = 10.0f;
     private float time = 1.0f;
 
-    private int vecX;
-    private int vecY;
+
     void Update()
     {
         //transform.LookAt(player.transform);//ここを編集したら玉がplayerに向かわなくなるはず
         time -= Time.deltaTime;
         if(time <= 0)
-        {
-           
-           vecX = Random.Range(-3,3);
-           vecY = Random.Range(-3,3);
+        { 
             BallShot();
-            time = 1.0f;//ballの速度
+            time = 0.1f;//ballの速度
         }
     }
- 
+
+    
+    private int vecX;
+    private int vecY; 
     void BallShot()
-    {
+    {    
+
+        vecX = Random.Range(-3,3);
+        vecY = Random.Range(-3,3);  
         //新しいボールを生成して、発射する
         GameObject shotObj = Instantiate(ball,new Vector3(vecX,vecY,5) , Quaternion.identity);
         shotObj.GetComponent<Rigidbody>().velocity = transform.forward * ballSpeed;
