9e6f029ee727e66303611f8bda606219b7fe9be0
u221honda <22111326ih@tama.ac.jp>
2023-09-23 11:02:59 +0900

    opencvsharp追加

---
+++ Assets/Script/FaceDector/FaceDector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVSharp;
+
+public class FaceDector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
