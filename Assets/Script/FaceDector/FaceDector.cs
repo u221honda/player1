00e31f43c27197eeaa2a87368cec1a77455c19a1
u221honda <22111326ih@tama.ac.jp>
2023-09-27 17:03:08 +0900

    顔の位置を取ることができたことと検出された顔に青い四角を入れることができた

--- Assets/Script/FaceDector/FaceDector.cs
+++ Assets/Script/FaceDector/FaceDector.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using OpenCVSharp;
+using OpenCvSharp;
 
 public class FaceDector : MonoBehaviour
 {
     // Start is called before the first frame update
+    WebCamTexture _WebCamTexture;
+    CascadeClassifier cascade;
+    OpenCvSharp.Rect MyFace;
     void Start()
     {
-        
+     WebCamDevice[] devices =  WebCamTexture.devices;
+
+     _WebCamTexture = new WebCamTexture(devices[0].name);
+     _WebCamTexture.Play();
+     cascade = new CascadeClassifier(Application.dataPath + @"/Script/FaceDector/haarcascade_frontalface_default.xml");   
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+void Update()
+{
+    GetComponent<Renderer>().material.mainTexture = _WebCamTexture;
+    Mat frame = OpenCvSharp.Unity.TextureToMat(_WebCamTexture);
+    findNewFace(frame);
+    display(frame);
+}
+
+    void findNewFace(Mat frame){
+        var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
+
+        if(faces.Length >= 1){
+           Debug.Log(faces[0].Location);
+           MyFace = faces[0];
+        }   
+    }
+    void display(Mat frame){
+        if (MyFace != null){
+            frame.Rectangle(MyFace,new Scalar(250,0,0),2);
+        }
+        Texture newtexture = OpenCvSharp.Unity.MatToTexture(frame);
+        GetComponent<Renderer>().material.mainTexture = newtexture;
     }
 }
