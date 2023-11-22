163f4c4127f021e4502a9ffa4b7be19468e8df3d
u221honda <22111326ih@tama.ac.jp>
2023-11-22 12:55:34 +0900

    とりあえずcamera前に設定できた

--- Assets/Script/FaceDector/FaceDector.cs
+++ Assets/Script/FaceDector/FaceDector.cs
@@ -1,53 +1,50 @@
 using System.Collections;
 using UnityEngine;
 using OpenCvSharp;
-using UnityEngine.UI;
 
 public class FaceDector : MonoBehaviour
 {
-    WebCamTexture _WebCamTexture; // ウェブカムの映像をテクスチャとして保持する変数
-    CascadeClassifier cascade; // Haar Cascade分類器を使用して顔を検出するための変数
-    OpenCvSharp.Rect MyFace; // 検出された顔の座標情報を保持する変数
-    GameObject placedObject; // 生成されたオブジェクトの参照を保持する変数
+    WebCamTexture _WebCamTexture;
+    CascadeClassifier cascade;
+    OpenCvSharp.Rect MyFace;
+    GameObject placedObject;
 
-    public GameObject objectToPlace; // オブジェクトのプレハブをアサインするための変数
+    public GameObject objectToPlace;
 
     void Start()
     {
-        // ウェブカムのデバイスを取得して、ウェブカムテクスチャを初期化・再生する.要するに映像取得やな
         WebCamDevice[] devices = WebCamTexture.devices;
         _WebCamTexture = new WebCamTexture(devices[0].name);
         _WebCamTexture.Play();
 
-        // Haar Cascade分類器を初期化する
         cascade = new CascadeClassifier(Application.dataPath + @"/Script/FaceDector/haarcascade_frontalface_default.xml");
     }
 
     void Update()
     {
-        // ウェブカムの映像をテクスチャとしてマテリアルに設定する。
         GetComponent<Renderer>().material.mainTexture = _WebCamTexture;
         Mat frame = OpenCvSharp.Unity.TextureToMat(_WebCamTexture);
 
-        // 顔を検出する
         findNewFace(frame);
 
-        // 検出された顔があればオブジェクトを顔から一定距離離れた位置に配置する
-        if (placedObject != null && MyFace != null)
+          // 検出された顔があればオブジェクトを顔と同じ座標に配置するようにしたい
+    if (placedObject != null && MyFace != null)
+    {
+        // 顔の中心座標を取得
+        Vector3 faceCenter = new Vector3(MyFace.X + MyFace.Width / 2, MyFace.Y + MyFace.Height / 2, 0);
+
+        // 顔の検出位置の200分の1にする
+        faceCenter /= 200f;
+        // Z座標を20に設定
+        faceCenter.z = 20f;
+        // オブジェクトの座標を設定
+        placedObject.transform.position = faceCenter;
+    }
+
+        // 青い四角を表示する
+        if (MyFace != null)
         {
-            // 顔の中心座標から一定距離離れた位置を計算する（ Z軸方向に200の位置に配置する）
-            Vector3 offsetFromFaceCenter = new Vector3(0, 0, 200);
-            Vector3 faceCenterWithOffset = new Vector3(MyFace.X + MyFace.Width / 2, MyFace.Y + MyFace.Height / 2, 0) + offsetFromFaceCenter;
-            placedObject.transform.position = faceCenterWithOffset;
-        }
-        else
-        {
-            // 顔が検出されていない場合、生成されたオブジェクトがあれば破棄する
-            if (placedObject != null)
-            {
-                Destroy(placedObject);
-                placedObject = null;
-            }
+            frame.Rectangle(MyFace, new Scalar(250, 0, 0), 2);
         }
 
         // OpenCVのMatをTextureに変換して表示する処理
@@ -57,23 +54,18 @@ public class FaceDector : MonoBehaviour
 
     void findNewFace(Mat frame)
     {
-        // Haar Cascade分類器を使用して顔を検出する
         var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
 
         if (faces.Length >= 1)
         {
             Debug.Log(faces[0].Location);
             MyFace = faces[0];
-            
+
             // 顔が検出されたらオブジェクトを生成する（一度だけ生成する）
             if (placedObject == null)
             {
                 placedObject = Instantiate(objectToPlace);
             }
-            
-            // オブジェクトを顔が検出された場所に移動する
-            Vector3 newPosition = new Vector3(MyFace.Location.X, MyFace.Location.Y, placedObject.transform.position.z);
-            placedObject.transform.position = newPosition;
         }
         else
         {
