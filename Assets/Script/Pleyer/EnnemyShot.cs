5c6ff9495d8924b20f1b2668b35e6db8bff19596
u221honda <22111326ih@tama.ac.jp>
2024-01-10 11:12:39 +0900

    o

---
+++ Assets/Script/Pleyer/EnnemyShot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject ball;
+    private float ballSpeed = 10.0f;
+    private float time = 1.0f;
+
+    private int vecX;
+    private int vecY;
+    void Update()
+    {
+        //transform.LookAt(player.transform);//ここを編集したら玉がplayerに向かわなくなるはず
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+           
+           vecX = Random.Range(-3,3);
+           vecY = Random.Range(-3,3);
+            BallShot();
+            time = 1.0f;//ballの速度
+        }
+    }
+ 
+    void BallShot()
+    {
+        //新しいボールを生成して、発射する
+        GameObject shotObj = Instantiate(ball,new Vector3(vecX,vecY,30) , Quaternion.identity);
+        shotObj.GetComponent<Rigidbody>().velocity = -transform.forward * ballSpeed;
+        //Objectを消す関数
+       Destroy(shotObj, 5f);                                                                       
+    }
+}
